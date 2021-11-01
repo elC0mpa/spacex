df826bd7cf21986c2e279ccbd0ce514c3940e3e9
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2021-11-01 02:09:48 -0400

    First steps in home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="home">
     <navigation-button></navigation-button>
+    <div class="home__info-container" v-if="!isLoading">
+      <p class="home__info-container--header">
+        Next launch: <span>{{ name }}</span>
+      </p>
+      <vue-count-down
+        :time="timeToLaunch"
+        v-slot="{ days, hours, minutes, seconds }"
+      >
+        Time Remaining：{{ days }} days, {{ hours }} hours,
+        {{ minutes }} minutes, {{ seconds }} seconds.
+      </vue-count-down>
+    </div>
   </div>
 </template>
 
@@ -8,16 +20,58 @@
 // @ is an alias to /src
 import NavigationButton from "@/components/NavigationButton";
 import { nextLaunchInfo } from "@/composables/api";
+import { reactive, toRefs } from "@vue/reactivity";
+import VueCountDown from "@chenfengyuan/vue-countdown";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 export default {
   name: "Home",
   components: {
     NavigationButton,
+    VueCountDown,
   },
   setup() {
-    const data = nextLaunchInfo();
-    console.log("Next launch info: ", data);
+    dayjs.extend(relativeTime);
+    const state = reactive({
+      name: "",
+      timeToLaunch: 0,
+      isLoading: true,
+    });
+
+    nextLaunchInfo()
+      .then((data) => {
+        state.name = data.name;
+        state.timeToLaunch = dayjs(data.date_local).diff(dayjs());
+        state.isLoading = false;
+      })
+      .catch((error) => {
+        console.log("Next launch error: ", error);
+      });
+
+    return { ...toRefs(state) };
   },
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.home {
+  background-image: url("../assets/images/background-home.jpeg");
+  background-position: right top;
+  width: 100vw;
+  height: 100vh;
+  color: $color-white;
+  &__info-container {
+    position: absolute;
+    top: 40%;
+    left: 50%;
+    transform: translateX(-50%);
+    &--header {
+      font-size: 5rem;
+      span {
+        color: $star-command-blue;
+        font-weight: bold;
+      }
+    }
+  }
+}
+</style>
