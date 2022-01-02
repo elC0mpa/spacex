1359b444b097056311abb4cfc34610e2024fcbf2
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2022-01-01 23:51:41 -0500

    Populating fileds in launchDetails request

--- src/composables/api.js
+++ src/composables/api.js
@@ -59,9 +59,46 @@ export const latestLaunchesInfo = (page, pageSize) => {
 export const launchDetails = (id) => {
   return new Promise(async (resolve, reject) => {
     try {
-      const { data } = await axios.get(`v5/launches/${id}`);
-      resolve(data);
+      const { data } = await axios.post("v5/launches/query", {
+        query: {
+          _id: id,
+        },
+        options: {
+          populate: [
+            {
+              path: "launchpad",
+              select: {
+                full_name: 1,
+                latitude: 1,
+                longitude: 1,
+                details: 1,
+                wikipedia: 1,
+              },
+            },
+            {
+              path: "ships",
+              select: {
+                name: 1,
+                latitude: 1,
+                longitude: 1,
+                active: 1,
+                image: 1,
+                launches: 1,
+                type: 1,
+              },
+            },
+            {
+              path: "rocket",
+              select: {
+                name: 1,
+              },
+            },
+          ],
+        },
+      });
+      resolve(data.docs[0]);
     } catch (error) {
+      console.error("ERROR: 💥 ", error);
       reject(error);
     }
   });
